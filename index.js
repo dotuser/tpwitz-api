15c02d288a8a7ae581dd3a84a9aaa08ee93eaa12
dotuser <ahmedhassaan@tutanota.com>
2024-10-18 23:24:26 +0500

    Vercel Reconfig

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const visitorRoutes = require('./routes/visitors');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const visitorRoutes = require("./routes/visitors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,19 +11,24 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  dbName: 'tpwitzDB'
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('MongoDB connection error:', err));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    dbName: "tpwitzDB",
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+app.get("/", (req, re) => {
+  res.status(200).send("Home");
+});
 
 // Routes
-app.use('/api/visitors', visitorRoutes);
+app.use("/api/visitors", visitorRoutes);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err);
-  res.status(500).send({ message: 'Internal Server Error' });
+  res.status(500).send({ message: "Internal Server Error" });
 });
 
 // Start the server
