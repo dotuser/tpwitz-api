99bb1d2023bb1b9fddb18178b77c9c2280121599
dotuser <ahmedhassaan@tutanota.com>
2024-10-19 11:42:23 +0500

    Code Reconfig for Vercel

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-app.get("/", (req, re) => {
+app.get("/", (req, res) => {
   res.status(200).send("Home");
 });
 
